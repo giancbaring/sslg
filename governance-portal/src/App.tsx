9539288281370912fe5cc028d2abe13c0bdb3d82
iac.sslg.dsdsnhs <iac.sslg.dsdsnhs@gmail.com>
2026-01-27 15:25:19 +0000

    1.2

--- governance-portal/src/App.tsx
+++ governance-portal/src/App.tsx
@@ -1,26 +1,11 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import NewsPage from './pages/NewsPage';
-import OfficersPage from './pages/OfficersPage';
-import DocumentsPage from './pages/DocumentsPage';
-import EventsPage from './pages/EventsPage';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Layout from './Layout';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="bg-gray-100 min-h-screen">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/officers" element={<OfficersPage />} />
-          <Route path="/documents" element={<DocumentsPage />} />
-          <Route path="/events" element={<EventsPage />} />
-        </Routes>
-      </div>
+      <Layout />
     </Router>
   );
 };
